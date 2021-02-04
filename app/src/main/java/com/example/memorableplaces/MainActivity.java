edf5e65f82f8f35995245a94bb732ddcb00a041c
Dmitrijevs <ddmitrijevs@gmail.com>
2021-02-04 17:52:44 +0200

    Data storage commit

--- app/src/main/java/com/example/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/memorableplaces/MainActivity.java
@@ -7,7 +7,9 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -25,12 +27,22 @@ public class MainActivity extends AppCompatActivity {
     static String addPlaceText = "Add a new place...";
     ArrayList<MemorablePlace> memorablePlaces = new ArrayList<MemorablePlace>();
     ListView placesListView;
-
+    SharedPreferences sharedPreferences;
    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = this.getSharedPreferences("com.example.memorableplaces.sharedpreferences", Context.MODE_PRIVATE);
+        try {
+            memorablePlaces = (ArrayList<MemorablePlace>) ObjectSerializer.deserialize(sharedPreferences.getString("list", ObjectSerializer.serialize(new ArrayList<MemorablePlace>())));
+            Log.i("Places", memorablePlaces.toString());
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            Log.i("Error", "error");
+        }
         placesListView = findViewById(R.id.listView);
         updateListView();
         placesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -52,13 +64,13 @@ public class MainActivity extends AppCompatActivity {
     }
    
     protected void updateListView(){
-        ArrayList<String> names = new ArrayList<String>();
-        for (MemorablePlace place : memorablePlaces) {
-            names.add(place.getName());
-        }
-        names.add(addPlaceText);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, names);
-        placesListView.setAdapter(arrayAdapter);
+            ArrayList<String> names = new ArrayList<String>();
+            for (MemorablePlace place : memorablePlaces) {
+                names.add(place.getName());
+            }
+            names.add(addPlaceText);
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, names);
+            placesListView.setAdapter(arrayAdapter);
     }
 
     @Override
@@ -75,6 +87,14 @@ public class MainActivity extends AppCompatActivity {
             }
             if(placeDouble == false) {
                 memorablePlaces.add(resultPlace);
+                try {
+                    sharedPreferences.edit().putString("list", ObjectSerializer.serialize(memorablePlaces)).apply();
+                    Log.i("Obj", ObjectSerializer.serialize(memorablePlaces));
+                }
+                catch(Exception e){
+                    Log.i("Error", "error");
+                    e.printStackTrace();
+                }
                 updateListView();
             }
         }
